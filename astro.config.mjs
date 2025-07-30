7b758b6878390de573cdef2b97ad985e743c615f
adog <adrianmg3008@gmail.com>
2025-07-30 12:31:11 -0400

    update  Astro configuration to integrate Tailwind CSS and adjust styles in components, removing unnecessary CSS and applying Tailwind classes instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adogdev.github.io',
   base: '/portfolio-cv',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
